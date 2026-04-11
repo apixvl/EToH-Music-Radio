33b7c051252e32594eae3b6cb1d4a79aff5ffbc8
apixvl <apixvlroblox@gmail.com>
2026-04-10 18:01:28 -0700

    Update stations.js
    github i hate you 😠

--- stations.js
+++ stations.js
@@ -1,4 +1,5 @@
-const allStations = [
+window.allStations = [
     { name: "Ring 1: Limbo", url: "index.html" },
-    { name: "Easter 2026: Elysian Fields", url: "easter2026.html" },
+    { name: "Easter 2026: Elysian Fields", url: "easter2026.html" }
 ];
+console.log("Stations.js has loaded the list:", window.allStations);
